da40e6f1bd426d3e80f29aa05dd507fe81a3da98
aproposmath <aproposmath@proton.me>
2025-12-01 21:05:46 +0100

    use StationeersMod, simplify toolchain

--- Main.cs
+++ Main.cs
@@ -1,31 +1,49 @@
 using System;
 using BepInEx;
 using HarmonyLib;
+using StationeersMods.Interface;
 
 namespace ExampleMod
 {
-    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    [StationeersMod(PluginGuid, PluginName, PluginVersion)]
+    [BepInPlugin(PluginGuid, PluginName, PluginVersion)]
     public class ExampleModPlugin : BaseUnityPlugin
     {
-        public const string pluginGuid = "aproposmath-stationeers-example-mod"; // Change this to your own unique Mod ID
-        public const string pluginName = "ExampleMod";
-        public const string pluginVersion = VersionInfo.Version;
+        public const string PluginGuid = "aproposmath-stationeers-example-mod"; // Change this to your own unique Mod ID
+        public const string PluginName = ThisAssembly.AssemblyName;
+        public const string PluginVersion = ThisAssembly.AssemblyVersion;
+        public const string PluginLongVersion = ThisAssembly.AssemblyInformationalVersion;
+
+        private Harmony _harmony = null;
 
         private void Awake()
         {
             try
             {
                 Logger.LogInfo(
-                    $"Awake ${pluginName} {VersionInfo.VersionGit}, build time {VersionInfo.BuildTime}"
+                    $"Awake {PluginName} {PluginGuid} {PluginLongVersion}"
                 );
 
-                var harmony = new Harmony(pluginGuid);
-                harmony.PatchAll();
+                _harmony = new Harmony(PluginGuid);
+                _harmony.PatchAll();
             }
             catch (Exception ex)
             {
-                Logger.LogError($"Error during ${pluginName} {VersionInfo.VersionGit} init: {ex}");
+                Logger.LogError($"Error in {PluginName} {PluginLongVersion} Awake: {ex}");
             }
+        }
+
+        private void OnDestroy()
+        {
+            Logger.LogInfo($"OnDestroy {PluginName} {PluginLongVersion}");
+
+            // Uncomment this if you are using BepInEx SriptEngine plugin for hot-reloading
+            // the mod while the game is running
+            // See https://github.com/BepInEx/BepInEx.Debug/blob/master/README.md#scriptengine
+            // But remove it again before building a release version of your mod, as sometimes this function is called immediately
+            // after Awake(), which would break your mod.
+
+            _harmony.UnpatchSelf();
         }
     }
 }
